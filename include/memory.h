a8fe83c7e1d69d9e87739894dd86a981834aceaf
Arman chauhan <ar.chauhan9863@gmail.com>
2024-10-22 23:09:33 +0545

    Basic structs

---
+++ include/memory.h
@@ -0,0 +1,39 @@
+#ifndef MEMORY_H
+#define MEMORY_H
+
+#include <stdint.h>
+
+#define MEM_SIZE 0x10000         // Total memory size (1 MB)
+#define ROM_BANK_SIZE 0x4000     // 16 KB per ROM bank
+#define ROM_NUM 2                // Number of ROM banks
+#define VRAM_SIZE 0x2000         // 8 KB for Video RAM
+#define EXTERNAL_RAM_SIZE 0x2000 // 8 KB for External RAM (SRAM)
+#define WRAM_SIZE 0x2000         // 8 KB for Work RAM
+#define ECHO_RAM_SIZE 0x1E00     // Echo RAM (7.25 KB)
+#define OAM_SIZE 0x00A0          // 160 bytes for Object Attribute Memory (OAM)
+#define UNUSED_SIZE 0x60         // 96 bytes of unused memory space
+#define IO_REGS_SIZE 0x0080      // 128 bytes for I/O registers
+#define HIGH_RAM_SIZE 0x007F     // 127 bytes for High RAM (HRAM)
+
+typedef union _mmu_t {
+    uint8_t addr[MEM_SIZE]; // Raw address access
+    struct {
+        uint8_t rom[ROM_NUM][ROM_BANK_SIZE]; // Cartridge ROM (multiple banks)
+        uint8_t vram[VRAM_SIZE];             // Video RAM (VAM)
+        uint8_t sram[EXTERNAL_RAM_SIZE];     // External RAM (SRAM)
+        uint8_t wram[WRAM_SIZE];             // Work RAM (WRAM)
+        uint8_t echo_ram[ECHO_RAM_SIZE];     // Echo RAM, mirror of work ram
+        uint8_t oam[OAM_SIZE];               // Object Attribute Memory (OAM)
+        uint8_t unused[UNUSED_SIZE];         // Unused memory space
+        uint8_t io_regs[IO_REGS_SIZE];       // I/O registers
+        uint8_t hram[HIGH_RAM_SIZE];         // High RAM (HRAM)
+        uint8_t ie_reg;                      // Interrupt Enable Register, 0xFFF
+    };
+} mmu_t;
+
+uint8_t read_u8(uint16_t addr);
+uint16_t read_u16(uint16_t addr);
+void write_u8(uint16_t addr, uint8_t data);
+void write_u16(uint16_t addr, uint16_t data);
+
+#endif
